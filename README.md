537e504cdc4da63ef1b7e8e93d9de54aeb6644b4
mobikarl <mobikarl@qq.com>
2022-12-19 15:41:06 +0800

    update

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 ## 示例
 使用calibre和nginx服务器下载书籍
 
+需要在阿里云香港区域配置启动模版（实例自定义数据中填入user-data.sh文件中的内容）和弹性伸缩组，获取实例的公有ip地址
+
 ```shell
 # 启动容器
 docker-compose up -d
@@ -10,4 +12,8 @@ http://ip:8080/
 
 # 访问nginx
 http://ip:80
+
+# 服务器启动+执行初始化命令，大概需要4分钟
+# 可登录服务器查看执行cloud-init命令的输出信息
+tail -f /var/log/cloud-init-output.log
 ```
--- user-data.sh
+++ user-data.sh
@@ -2,7 +2,12 @@
 yum install -y yum-utils git
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+echo "action zero" | tee /root/zero.log
 systemctl start docker
+echo "action one" | tee /root/one.log
+pwd
 git clone https://github.com/mobikarl/calibre-docker.git
+echo "action two" | tee /root/two.log
 cd calibre-docker
+echo "action three" | tee /root/three.log
 docker compose up -d
